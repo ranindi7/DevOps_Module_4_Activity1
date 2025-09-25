4b2302fff4d1042064cb1f02be756960f0b04220
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2025-09-25 14:12:38 -0500

    Fixed broken f string

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def index():
     if request.method == 'POST':
         name = request.form.get('name', 'Guest')
         ip_address = get_container_ip()
-        greeting = f"Hello {name}, I am greeting you from a
-Python container with IP address {ip_address}"
+        greeting = f"Hello {name}, I am greeting you from a Python container with IP address {ip_address}"
         return render_template('index.html', greeting=greeting)
     return render_template('index.html', greeting=None)
